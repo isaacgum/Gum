a95b5bd2b21bca98ddd2841d2e903ee825bca723
Isaac Gum <gumdeng902@gmail.com>
2026-03-07 02:42:13 +0200

    my first movies app

--- GUM/React/src/App.jsx
+++ GUM/React/src/App.jsx
@@ -1,35 +1,71 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
+import {useState, useEffect} from 'react';
+import './App.css';
+import SearchIcon from './search.svg';
+import MovieCard from './MovieCard';
+//67e5d50
+const API_URL = 'https://www.omdbapi.com?apikey=67e5d50';
 
-function App() {
-  const [count, setCount] = useState(0)
+const movie1 = {
+"Poster": "https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg",
+
+"Title": "Inception",
+
+"Type": "movie",
+
+"Year": "2010",
+
+"imdbID": "tt1375666",
+}
+const App = () => {
+
+  const [movies, setMovies] = React.useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
+
+const searchMovies = async (title) => {
+const responese = await fetch(`${API_URL}&s=${title}`);
+const data = await responese.json();
+
+setMovies(data.Search);
+}
+    
+useEffect(() => {
+searchMovies('Inception');
+}, []);
+
+
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+     <div className="app">
+        <h1>MovieLand</h1>
 
-export default App
+        <div className="search">
+          <input
+            placeholder="Search for movies"
+            value= {searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+          />
+          <img
+          src={SearchIcon}
+          alt="search"
+          onClick={() => searchMovies(searchTerm)}
+          />
+          </div>
+          {movies?.length > 0
+          ? (
+            <div className="container">
+              {movies.map((movie) => (
+                <MovieCard movie={movie} />
+              ))}
+            </div>
+          ):(
+            <div className="empty">
+              <h2>No movies found</h2>
+            </div>
+          )
+          }
+          
+        </div>
+  );
+}
+export default App;
